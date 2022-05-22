f1a027f6450fce43fad5ea8b066fa7133b46d6a2
TrieKai <trie55432@gmail.com>
2022-05-22 23:31:33 +0800

    feat(constant): add constant.ts

---
+++ assets/constant/constant.ts
@@ -0,0 +1,10 @@
+export const KKBOX_CHANNEL_ID = 'UCWjDbk8VIEc_OYqsAyal-Cw'
+
+export const YT_SEARCH_ENDPOINT =
+  'https://youtube.googleapis.com/youtube/v3/search'
+
+export const YT_VIDEOS_ENDPOINT =
+  'https://youtube.googleapis.com/youtube/v3/videos'
+
+export const DARK = 'dark'
+export const LIGHT = 'light'
