da7d672d1274b00ba8636d5f5317cbcb2250cba8
TrieKai <trie55432@gmail.com>
2022-05-23 21:41:59 +0800

    Deploy Next.js to gh-pages

--- _next/static/iBbebxMYJefgfRy497FVb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/749-2e07d41ab6643c72.js","static/chunks/pages/index-94646bbabeb38ead.js"],"/_error":["static/chunks/pages/_error-777a5f0e462a9bdf.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
